f7283f467c07ccca1eabb868683af6c6ae48433f
Harrison <Isaiah_Harrison@comcast.com>
2017-11-20 08:04:46 -0500

    init js services

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const rpg = angular.module('rpg',[]);
 
-rpg.controller('rpgCtr', ['$scope', function($scope) {
+rpg.controller('rpgCtr', ['$scope', 'character', function($scope, character) {
   $scope.rpg = 'rpg';
 }]);
+
+rpg.service('character', ['stats', function(stats){
+  this.getStatsFor = (characterName) => {
+    
+  }
+}]);
+
+rpg.service('stats', function(){
+  // this.forCharacter = ()
+})
